d0407860057cccbbbf16b8f95dfd39baeacf6234
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-07-10 20:13:13 +0530

    Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -24,79 +24,79 @@ class MyApp extends StatelessWidget {
                 end: Alignment.bottomRight,
               ),
             ),
-            child: businesscard(),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget businesscard() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const CircleAvatar(
-          radius: 60,
-          backgroundImage: AssetImage('images/Atharva.jpg'),
-        ),
-        Text(
-          'Atharva Paradkar',
-          style: GoogleFonts.pacifico(
-            textStyle: const TextStyle(
-              fontSize: 35,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Text(
-          'Flutter Developer',
-          style: GoogleFonts.sourceSans3(
-            textStyle: const TextStyle(
-              fontSize: 28,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 20,
-          width: 200,
-          child: Divider(color: Colors.deepPurple.shade200),
-        ),
-        Card(
-          elevation: 15,
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Padding(
-            padding: const EdgeInsets.all(14),
-            child: Row(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.phone,
-                  color: Colors.deepPurple[700],
+                const CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('images/Atharva.jpg'),
                 ),
-                const SizedBox(width: 30),
                 Text(
-                  '9822975185',
-                  style: TextStyle(fontSize: 16, color: Colors.deepPurple[700]),
+                  'Atharva Paradkar',
+                  style: GoogleFonts.pacifico(
+                    textStyle: const TextStyle(
+                      fontSize: 35,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
+                Text(
+                  'Flutter Developer',
+                  style: GoogleFonts.sourceSans3(
+                    textStyle: const TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(color: Colors.deepPurple.shade200),
+                ),
+                Card(
+                  elevation: 15,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(14),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          color: Colors.deepPurple[700],
+                        ),
+                        const SizedBox(width: 30),
+                        Text(
+                          '9822975185',
+                          style: TextStyle(
+                              fontSize: 16, color: Colors.deepPurple[700]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 15,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.deepPurple[700],
+                    ),
+                    title: Text(
+                      'atharvaparadkar91@gmail.com',
+                      style: TextStyle(
+                          fontSize: 16, color: Colors.deepPurple[700]),
+                    ),
+                  ),
+                )
               ],
             ),
           ),
         ),
-        Card(
-          elevation: 15,
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: ListTile(
-            leading: Icon(
-              Icons.email,
-              color: Colors.deepPurple[700],
-            ),
-            title: Text(
-              'atharvaparadkar91@gmail.com',
-              style: TextStyle(fontSize: 16, color: Colors.deepPurple[700]),
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
